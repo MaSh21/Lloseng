aabd2b23acbdb8c8a52a6750c76284506b34b5de
MaSh21 <65473432+MaSh21@users.noreply.github.com>
2020-05-30 14:16:09 -0400

    E6(b): User input for server.

--- code/simplechat1/EchoServer.java
+++ code/simplechat1/EchoServer.java
@@ -3,6 +3,7 @@
 // license found at www.lloseng.com 
 
 import java.io.*;
+import common.*;
 import ocsf.server.*;
 
 /**
@@ -17,12 +18,10 @@ import ocsf.server.*;
  */
 public class EchoServer extends AbstractServer 
 {
-  //Class variables *************************************************
-  
-  /**
-   * The default port to listen on.
-   */
-  final public static int DEFAULT_PORT = 5555;
+	
+	// Instance variables ******************************************
+	
+	ChatIF serverUI;
   
   //Constructors ****************************************************
   
@@ -31,13 +30,27 @@ public class EchoServer extends AbstractServer
    *
    * @param port The port number to connect on.
    */
-  public EchoServer(int port) 
+  public EchoServer(int port, ChatIF serverUI) throws IOException
   {
     super(port);
+	this.serverUI = serverUI;
+	listen();
   }
+
 
   
   //Instance methods ************************************************
+  
+  /**
+   * This method handles any messages received from the end-user of the server.
+   *
+   * @param msg The message received from the end-user.
+   */
+  public void handleMessageFromServerUI (String message)throws IOException {
+	serverUI.display("YOU TYPED: " + message);
+	String serverMsg = "SERVER MSG> " + message;
+	this.sendToAllClients(serverMsg);	  
+  }
   
   /**
    * This method handles any messages received from the client.
@@ -91,38 +104,5 @@ public class EchoServer extends AbstractServer
       ("Server has stopped listening for connections.");
   }
   
-  //Class methods ***************************************************
-  
-  /**
-   * This method is responsible for the creation of 
-   * the server instance (there is no UI in this phase).
-   *
-   * @param args[0] The port number to listen on.  Defaults to 5555 
-   *          if no argument is entered.
-   */
-  public static void main(String[] args) 
-  {
-    int port = 0; //Port to listen on
-
-    try
-    {
-      port = Integer.parseInt(args[0]); //Get port from command line
-    }
-    catch(Throwable t)
-    {
-      port = DEFAULT_PORT; //Set port to 5555
-    }
-	
-    EchoServer sv = new EchoServer(port);
-    
-    try 
-    {
-      sv.listen(); //Start listening for connections
-    } 
-    catch (Exception ex) 
-    {
-      System.out.println("ERROR - Could not listen for clients!");
-    }
-  }
 }
 //End of EchoServer class
---
+++ code/simplechat1/ServerConsole.java
@@ -0,0 +1,130 @@
+// This file contains material supporting section 3.7 of the textbook:
+// "Object Oriented Software Engineering" and is issued under the open-source
+// license found at www.lloseng.com 
+
+import java.io.*;
+import client.*;
+import common.*;
+
+/**
+ * This class constructs the UI for a server client.  It implements the
+ * chat interface in order to activate the display() method.
+ * Warning: Some of the code here is cloned from ClientConsole 
+ *
+ * @author Mariam Shlepchyan
+ */
+public class ServerConsole implements ChatIF 
+{
+  //Class variables *************************************************
+  
+  /**
+   * The default port to connect on.
+   */
+  final public static int DEFAULT_PORT = 5555;
+  
+  //Instance variables **********************************************
+  
+  /**
+   * The instance of the client that created this ServerConsole.
+   */
+  EchoServer server;
+
+  
+  //Constructors ****************************************************
+
+  /**
+   * Constructs an instance of the ServerConsole UI.
+   *
+   * @param host The host to connect to.
+   * @param port The port to connect on.
+   */
+  
+  public ServerConsole(int port) 
+  {
+    try 
+    {
+      server= new EchoServer(port, this);
+    } 
+    catch(IOException exception) 
+    {
+      System.out.println("Error: Can't setup connection!"
+                + " Terminating server.");
+      System.exit(1);
+    }
+  }
+
+  
+  //Instance methods ************************************************
+  
+  /**
+   * This method waits for input from the console.  Once it is 
+   * received, it sends it to the server's message handler.
+   */
+  public void accept() 
+  {
+    try
+    {
+      BufferedReader fromConsole = 
+        new BufferedReader(new InputStreamReader(System.in));
+      String message;
+
+      while (true) 
+      {
+        message = fromConsole.readLine();
+        server.handleMessageFromServerUI(message);
+      }
+    } 
+    catch (Exception ex) 
+    {
+      System.out.println
+        ("Unexpected error while reading from console!");
+    }
+  }
+
+  /**
+   * This method overrides the method in the ChatIF interface.  It
+   * displays a message onto the screen.
+   *
+   * @param message The string to be displayed.
+   */
+  public void display(String message) 
+  {
+    System.out.println("> " + message);
+  }
+
+  
+//Class methods ***************************************************
+  
+  /**
+   * This method is responsible for the creation of 
+   * the server UI.
+   *
+   * @param args[0] The port number to listen on.  Defaults to 5555 
+   *          if no argument is entered.
+   */
+  public static void main(String[] args) 
+  {
+    int port = 0; //Port to listen on
+
+    try
+    {
+      port = Integer.parseInt(args[0]); //Get port from command line
+    }
+    catch(Throwable t)
+    {
+      port = DEFAULT_PORT; //Set port to 5555
+    }
+	
+    ServerConsole sv = new ServerConsole(port);
+    
+    try 
+    {
+      sv.accept(); //Start listening for
+    } 
+    catch (Exception ex) 
+    {
+      System.out.println("ERROR - Could not listen for clients!");
+    }
+  }
+}
+//End of ConsoleChat class
